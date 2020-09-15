b3a734f09e640970118f8780f6fb706197a32e63
nickidahl <6417280@rts-365.dk>
2020-09-15 13:30:29 +0200

    videre

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { render } from "react-dom";
 import Header from "./Header";
-import Nav from "./Nav";
 import Main from "./Main";
 import Footer from "./Footer";
 
 const App = () => {
   return (
-    <div>
+    <Fragment>
       <Header />
-      <Nav />
       <Main />
       <Footer />
-    </div>
+    </Fragment>
   );
 };
 render(<App />, document.querySelector("#root"));
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Nav from "./Nav";
+import "./Header.scss";
+
 const Header = () => {
-  return <header>This is the header</header>;
+  return (
+    <header className="myheader">
+      <p className="myheader__text">This is the Header with a Nav</p>
+      <Nav></Nav>
+    </header>
+  );
 };
 export default Header;
